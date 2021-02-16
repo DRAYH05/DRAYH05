361928b5b0365e10805d2566e02005ee64554483
Mario Sánchez <mario1999sanchez@gmail.com>
2021-02-16 16:10:44 +0100

    Menu

--- pag_Follow.py
+++ pag_Follow.py
@@ -1,7 +1,8 @@
-print ('Usuario: ') #cuando inicie sesion aqui estara el usuario
-print ('[1] Login') 
-print ('[2] Crear cuenta')
-print ('[3] Ver hilos')
-print ('[4] Nuevo hilo')
-print ('[0] Salir')
-print ('elija opcion: ')
+# cuando inicie sesion aqui estara el usuario
+print('Usuario: ') 
+print('[1] Login')
+print('[2] Crear cuenta')
+print('[3] Ver hilos')
+print('[4] Nuevo hilo')
+print('[0] Salir')
+print('elija opcion: ')
--- pag_Unfollow.py
+++ pag_Unfollow.py
@@ -2,4 +2,24 @@ print('[1] Login')
 print('[2] Crear cuenta')
 print('[3] Ver hilos')
 print('[0] Salir')
-print('elija opcion: ')
+opcion = int(input('Elija una opcion: '))
+
+if opcion == 1:
+    Usuario = input('Usuario: ')
+    Contraseña = input('Contraseña: ')
+    if  Usuario == 1: #Usuario o contraseña incorrectos
+        print('Usuario o contraseña incorrectos')
+    else:
+        print('Usuario: ')
+        print('[1] Login')
+        print('[2] Crear cuenta')
+        print('[3] Ver hilos')
+        print('[4] Nuevo hilo')
+        print('[0] Salir')
+        print('elija opcion: ')
+
+
+if opcion == 2:
+    Usuario = int(input('Usuario: '))
+    Contraseña = int(input('Contraseña: '))
+    Contraseña1 = int(input('Repite contraseña: '))
