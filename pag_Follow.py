fc52735b0325807c7f16a76c845b7910273c7ed7
god <kevin677@hotmail.es>
2021-03-13 18:12:36 +0100

    Delete pag_Follow.py

--- pag_Follow.py
+++
@@ -1,8 +0,0 @@
-# cuando inicie sesion aqui estara el usuario
-print('Usuario: ') 
-print('[1] Login')
-print('[2] Crear cuenta')
-print('[3] Ver hilos')
-print('[4] Nuevo hilo')
-print('[0] Salir')
-print('elija opcion: ')
