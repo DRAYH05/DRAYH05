51338e2e75823a6bab7a59bcca10db0305505fd6
god <kevin677@hotmail.es>
2021-03-13 19:32:49 +0100

    Update AdminMenu.py

--- AdminMenu.py
+++ AdminMenu.py
@@ -3,7 +3,6 @@ import os
 
 def menu():
     os.system('cls')
-    print('Usuario: ')
     print('[1] Login')
     print('[2] Crear cuenta')
     print('[3] Ver hilos')
@@ -11,7 +10,15 @@ def menu():
     print('[0] Salir')
 
 
-def menu_hilos_Admin():
+def menu_loged():
+    os.system('cls')
+    print('Usuario: 'User)
+    print('[2] Crear cuenta')
+    print('[3] Ver hilos')
+    print('[4] Nuevo hilo')
+    print('[0] Salir')
+
+def menu_hilos():
     # comentario 1
     # comentario 2
     print('[1] Nuevo Comentario')
@@ -28,20 +35,20 @@ while not Salir:
     OpcionAdmin = int(input('Elija una opcion: '))
 
     if OpcionAdmin == 1:
-        User = input('Usuario: ')
-        password = input('Contraseña: ')
+        User = str(input('Usuario: '))
+        password = str(input('Contraseña: '))
 
 
     elif OpcionAdmin == 2:
-        User = input('Usuario: ')
-        password = input('Contraseña: ')
-        password1 = input('Repite contraseña: ')
+        User = str(input('Usuario: '))
+        password = str(input('Contraseña: '))
+        password1 = str(input('Repite contraseña: '))
 
     elif OpcionAdmin == 3:
         Salir_hilos = False
         os.system('cls')
         while not Salir_hilos:
-            menu_hilos_Admin()
+            menu_hilos()
             opcion1 = int(input('Elija una opcion: '))
             if opcion1 == 0:
                 Salir_hilos = True
