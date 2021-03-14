66b69377e75cc4bfe2661197227f764a847b7d5c
god <kevin677@hotmail.es>
2021-03-15 00:02:57 +0100

    Update pro_apy.py

--- pro_apy.py
+++ pro_apy.py
@@ -1,9 +1,7 @@
 from flask import Flask, jsonify, request, abort
 import json
-import time
-from datetime import datetime, timedelta
-from functools import wraps
 import pymongo
+from werkzeug.security import generate_password_hash, check_password_hash
 from bson import json_util
 from bson.objectid import ObjectId
 app = Flask(__name__)
@@ -22,22 +20,42 @@ def root():
 
 
 def obtener_id():
+    print('vuelva a introducir sus credenciales: ')
+    username = request.json['User']
+    password = request.json['pass']
+    if username and password:
+        myquery = { 'user': username, "pass": password}
+        x = tabla.find_one(myquery)
+        return jsonify({}), 200
+        if(x == None):
+            return jsonify({'ERROR': 'usuario no encontrado'}), 400
+        else:
+            id = tabla.find_one(myquery)
+            return jsonify({'sesion actual':username}), 200
+    else:   
+        return jsonify({'ERROR': 'rellene todos los campos'}), 400
     
 
 def comprobar_admin():
-    query = {'user': username, 'pass': password}
+    username = request.json['User']
+    password = request.json['pass']
+    query = {'User': username, 'pass': password}
     projection = {'is_admin': 1}
     result = mydb.Usuarios.find_one(query, projection)
-    if(result == True):
-      print(result)
+    #if (result == true):
+        #........
+    
 
 @app.route('/crear_usuario', methods=['POST'])
 def crear_usuarios():
     username = request.json['User']
     password = request.json['pass']
     if username and password:
-        x = tabla_users.insert({'User':username, 'pass':password, 'is_admin': False})
-        return jsonify({'usuario creado'}), 200
+        if (tabla_users.find({'User':username}, {'User': 1}) == None):
+            x = tabla_users.insert({'User':username, 'pass':password, 'is_admin': False})
+            return jsonify({'usuario creado'}), 200
+        else:
+            return jsonify({'usuario repetido'}), 400
     else:
         return jsonify({'ERROR': 'la creacion del usuario especificado no fue posible'}), 400
 
@@ -73,12 +91,37 @@ def delete_hilo(id):
 
 @app.route('/creacion_hilo', methods=['POST'])
 def crear_hilo():
-    
+    obtener_id()
+    nombre_hilo = request.json['nombre']
+    contenido = request.json['contenido']
+    if nombre_hilo and contenido:
+        x = tabla_hilos.insert({'nombre':nombre_hilo, 'id_usuario': id, 'contenido': contenido})
+        return jsonify({'hilo creado'}), 200
+    else:
+        return jsonify({'ERROR': 'la creacion del hilo no fue posible'}), 400
 
+@app.route('/visualizar_comentarios', methods=['GET'])
+def ver_comentarios():
+    ver_hilo()
+    comentario = tabla_comentarios.find({'id_hilo': ObjectId(id)})
+    for x in comentario:
+        print(x)
+    return jsonify({}), 200
 
+@app.route('/creacion_comentario', methods=['POST'])
+def crear_comentario():
+    ver_hilo()
+    contenido_comentario = request.json['contenido']
+    if contenido_comentario:
+        x = tabla_comentarios.insert({'id_hilo': id, 'Ccontenido': contenido_comentario})
+        return jsonify({'comentario creado'}), 200
+    else:
+        return jsonify({'ERROR': 'la creacion del comentario no fue posible'}), 400
 
-
-
+@app.route('/borrar_comentario/<id>', methods=['DELETE'])
+def delete_comentario(id):
+    comentario = tabla_comentarios.delete_one({'_id': ObjectId(id)})
+    return jsonify({'peticion aceptada': 'usuario eliminado'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
